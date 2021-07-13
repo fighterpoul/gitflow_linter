75e86e5e1762db30236d46e200d5057253000449
Poul Fighter <fighter.poul@gmail.com>
2021-07-13 18:46:09 +0200

    Bump up version to 0.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Poul Fighter'
 author = 'Poul Fighter'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 # -- General configuration ---------------------------------------------------
 
